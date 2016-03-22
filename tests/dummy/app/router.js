96a4394beca5c1725c7581d68a55ce9af7759e60
Roxanne Panthaky <rpanthak@ciena.com>
2016-03-22 13:46:38 -0400

    added froxt-text to core

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -9,6 +9,10 @@ Router.map(function () {
   this.route('demo', { path: '/' })
   this.route('checkbox')
   this.route('icons')
+  this.route('text-input', function () {
+    this.route('field', { path: '/field' })
+    this.route('area', { path: '/area' })
+  })
 })
 
 export default Router
