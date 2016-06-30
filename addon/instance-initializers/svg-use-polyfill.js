eade3af7e741c53fe13c73d79640678e58cab5cf
Eric White <--global>
2016-06-30 11:57:01 -0400

    Inline svg config option

--- addon/instance-initializers/svg-use-polyfill.js
+++ addon/instance-initializers/svg-use-polyfill.js
@@ -1,7 +1,14 @@
 import svg4everybody from 'npm:svg4everybody'
-
+import config from 'ember-get-config'
 export function initialize () {
-  svg4everybody()
+  if (config.iconPacks && config.iconPacks.inline) {
+    svg4everybody({
+      nosvg: true, // shiv <svg> and <use> elements and use image fallbacks
+      polyfill: true // polyfill <use> elements for External Content
+    })
+  } else {
+    svg4everybody()
+  }
 }
 
 export default {
