e53c80034eab1d16e509c61bcffdd87cd228669b
Eric White <--global>
2016-06-30 15:33:30 -0400

    Change configuration option location

--- addon/instance-initializers/svg-use-polyfill.js
+++ addon/instance-initializers/svg-use-polyfill.js
@@ -1,7 +1,8 @@
 import svg4everybody from 'npm:svg4everybody'
-import config from 'ember-get-config'
+import Ember from 'ember'
+const {ENV} = Ember
 export function initialize () {
-  if (config.iconPacks && config.iconPacks.inline) {
+  if (ENV.iconPacks && ENV.iconPacks.inline) {
     svg4everybody({
       nosvg: true, // shiv <svg> and <use> elements and use image fallbacks
       polyfill: true // polyfill <use> elements for External Content
