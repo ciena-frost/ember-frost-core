fa17a96816dca057cb8d27026bbc4c3c538e11d3
Seena <seenarowhani95@gmail.com>
2016-07-02 11:50:00 -0400

    added test

--- addon/components/frost-radio-button.js
+++ addon/components/frost-radio-button.js
@@ -1,6 +1,7 @@
 import Ember from 'ember'
 import layout from '../templates/components/frost-radio-button'
 export default Ember.Component.extend({
+  classNames: ['frost-radio-button'],
   layout,
   actions: {
     handleChange (value) {
---
+++ tests/integration/components/frost-radio-button-test.js
@@ -0,0 +1,37 @@
+import Ember from 'ember';
+import {
+  expect
+} from 'chai'
+import {
+  describeComponent,
+  it
+} from 'ember-mocha';
+import hbs from 'htmlbars-inline-precompile';
+
+const {
+  run
+} = Ember;
+
+describeComponent('frost-radio-button', 'FrostRadioButtonComponent', {
+  integration: true
+}, function() {
+  it('Action fires correctly', function() {
+    this.on('changed', (value) => {
+      expect(value).to.equal('test')
+    })
+    this.render(hbs `
+      {{frost-radio-button
+          groupValue=groupValue
+          value='test'
+          changed='changed'
+      }}
+    `);
+
+    expect(this.$('input').prop('checked')).to.equal(false)
+    run(() => {
+      this.$('input').trigger('click');
+    });
+    expect(this.$('input').prop('checked')).to.equal(true)
+    expect(this.get('groupValue')).to.equal('test')
+  });
+})
