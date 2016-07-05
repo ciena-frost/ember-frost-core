793f6d0932b15c175a2a8de89ae7f62b5316f194
Steven Glanzer <sglanzer@ciena.com>
2016-07-05 11:29:55 -0400

    Changing the event flow

--- addon/components/frost-radio-button.js
+++ addon/components/frost-radio-button.js
@@ -1,37 +1,50 @@
 import Ember from 'ember'
+const {
+  Component,
+  computed
+} = Ember
+const {
+  readOnly
+} = computed
 import layout from '../templates/components/frost-radio-button'
-export default Ember.Component.extend({
-  classNames: ['frost-radio-button'],
+
+export default Component.extend({
+  attributeBindings: [
+    'tabindex'
+  ],
+  classNames: [
+    'frost-radio-button'
+  ],
   classNameBindings: [
     'checked',
     'disabled',
     'required'
   ],
-  attributeBindings: ['tabindex'],
   layout,
+  // TODO PropTypes
   required: false,
   disabled: false,
+
+  groupId: readOnly('parentView.id'),
+  groupValue: readOnly('parentView.value'),
+  onChange: readOnly('parentView.onChange'),
+
+  checked: computed('groupValue', 'value', function () {
+    return this.get('groupValue') === this.get('value')
+  }),
+
   tabindex: Ember.computed('disabled', function () {
     return this.get('disabled') ? -1 : 0
   }),
-  group: Ember.computed.alias('parentView.id'),
-  onChange: Ember.computed.alias('parentView.onChange'),
+
   init () {
     this._super(...arguments)
     Ember.assert(
       `${this.toString()} must be initialized in the yield block of 'frost-radio-group'`,
       /frost-radio-group/.test(this.parentView.toString()))
     Ember.assert(
-      `${this.toString()} must be initialized with a truthey 'value' property`,
+      `${this.toString()} must be initialized with a 'value' property`,
       this.get('value')
     )
-  },
-  actions: {
-    onChange (value) {
-      let handler = this.get('onChange')
-      if (handler && typeof handler === 'function') {
-        handler(value)
-      }
-    }
   }
 })
