eba554a4d91abfe5429ee0f745943f0e36ec5bea
Seena <seenarowhani95@gmail.com>
2016-07-11 15:59:14 -0400

    added files to eslint ignore, eslint of frost-component index.js

--- blueprints/frost-component/index.js
+++ blueprints/frost-component/index.js
@@ -1,12 +1,12 @@
-/*jshint node:true*/
+/* jshint node:true*/
 
-var stringUtil         = require('ember-cli-string-utils');
-var pathUtil           = require('ember-cli-path-utils');
-var validComponentName = require('ember-cli-valid-component-name');
-var getPathOption      = require('ember-cli-get-component-path-option');
-var path               = require('path');
+var stringUtil = require('ember-cli-string-utils')
+var pathUtil = require('ember-cli-path-utils')
+var validComponentName = require('ember-cli-valid-component-name')
+var getPathOption = require('ember-cli-get-component-path-option')
+var path = require('path')
 
-var normalizeEntityName = require('ember-cli-normalize-entity-name');
+var normalizeEntityName = require('ember-cli-normalize-entity-name')
 
 module.exports = {
   description: 'Generates a frost component. Name must contain a hyphen.',
@@ -16,67 +16,67 @@ module.exports = {
       name: 'path',
       type: String,
       default: 'components',
-      aliases:[
+      aliases: [
         {'no-path': ''}
       ]
     }
   ],
-  fileMapTokens: function() {
+  fileMapTokens: function () {
     return {
-      __path__: function(options) {
+      __path__: function (options) {
         if (options.pod) {
-          return path.join(options.podPath, options.locals.path, options.dasherizedModuleName);
+          return path.join(options.podPath, options.locals.path, options.dasherizedModuleName)
         }
-        return 'components';
+        return 'components'
       },
-      __templatepath__: function(options) {
+      __templatepath__: function (options) {
         if (options.pod) {
-          return path.join(options.podPath, options.locals.path, options.dasherizedModuleName);
+          return path.join(options.podPath, options.locals.path, options.dasherizedModuleName)
         }
-        return 'templates/components';
+        return 'templates/components'
       },
-      __templatename__: function(options) {
+      __templatename__: function (options) {
         if (options.pod) {
-          return 'template';
+          return 'template'
         }
-        return options.dasherizedModuleName;
+        return options.dasherizedModuleName
       }
-    };
+    }
   },
 
-  normalizeEntityName: function(entityName) {
-    entityName = normalizeEntityName(entityName);
-    return validComponentName(entityName);
+  normalizeEntityName: function (entityName) {
+    entityName = normalizeEntityName(entityName)
+    return validComponentName(entityName)
   },
-  locals: function(options) {
-    options.originBlueprintName = 'component';
-    var templatePath   = '';
-    var importTemplate = '';
-    var contents = '';
+  locals: function (options) {
+    options.originBlueprintName = 'component'
+    var templatePath = ''
+    var importTemplate = ''
+    var contents = ''
     if (options.project.isEmberCLIAddon() || !options.inDummy) {
       if (options.pod) {
-        templatePath   = './template';
+        templatePath = './template'
       } else {
-        templatePath   = pathUtil.getRelativeParentPath(options.entity.name) +
-          'templates/components/' + stringUtil.dasherize(options.entity.name);
+        templatePath = pathUtil.getRelativeParentPath(options.entity.name) +
+          'templates/components/' + stringUtil.dasherize(options.entity.name)
       }
-      importTemplate   = 'import layout from \'' + templatePath + '\';\n';
+      importTemplate = 'import layout from \'' + templatePath + '\';\n'
       contents +=
-        '\n\t// == Properties ============================================================='  +
-        '\n\texcludeEvents: [],'  +
-        '\n\tclassNames: [\n\t\t\'' + options.entity.name + '\'\n\t],'  +
-        '\n\tlayout,'             +
+        '\n\t// == Properties =============================================================' +
+        '\n\texcludeEvents: [],' +
+        '\n\tclassNames: [\n\t\t\'' + options.entity.name + '\'\n\t],' +
+        '\n\tlayout,' +
         '\n\tpropTypes: {},'
       contents +=
-        '\n\t// == Functions ============================================================='  +
-        '\n\tgetDefaultProps () {'+
-          '\n\t\treturn {}'       +
-        '\n\t}';
+        '\n\t// == Functions =============================================================' +
+        '\n\tgetDefaultProps () {' +
+          '\n\t\treturn {}' +
+        '\n\t}'
     }
     return {
       importTemplate,
       contents,
       path: getPathOption(options)
-    };
+    }
   }
-};
+}
