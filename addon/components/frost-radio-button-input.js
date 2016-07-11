b24ebc8b2c3c4db46f3dab5eda4f311edbd2da20
Seena <seenarowhani95@gmail.com>
2016-07-11 10:00:58 -0400

    removed blueprint and coding standard

--- addon/components/frost-radio-button-input.js
+++ addon/components/frost-radio-button-input.js
@@ -14,22 +14,21 @@ export default Component.extend(FrostEvents, {
     'type'
   ],
   classNames: ['frost-radio-button-input'],
+  excludeEvents: ['onChange'],
   tagName: 'input',
   type: 'radio',
+
   checked: computed('groupValue', 'value', function () {
     return this.get('groupValue') === this.get('value')
   }),
 
-  excludeEvents: ['onChange'],
   init () {
-
     this._super(...arguments)
 
-    Ember.assert(
-      `${this.toString()} must be initialized in the yield block of 'frost-radio-button'`,
-      /frost-radio-button/.test(this.parentView.toString()))
+    let assert = `${this.toString()} must be initialized in the yield block of 'frost-radio-button'`
+    let cond   = /frost-radio-button/.test(this.parentView.toString())
+    Ember.assert(assert, cond)
   },
-
   change (event) {
     if (this.onChange && typeof this.onChange === 'function') {
       this.onChange(this.parentView._createEvent(event, event.target))
