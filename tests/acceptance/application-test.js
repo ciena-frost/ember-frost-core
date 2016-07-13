fad1214e2d54f891a63b7c1a4a7daae19f3f3638
Eric White <--global>
2016-07-13 10:43:10 -0400

    Use done() + increase timeout on icons

--- tests/acceptance/application-test.js
+++ tests/acceptance/application-test.js
@@ -29,84 +29,134 @@ describe('Acceptance: Application', function () {
       expect(currentPath()).to.equal('demo')
     })
   })
-  it('can visit /palette', function () {
+  it('can visit /palette', function (done) {
     visit('/palette')
 
     andThen(function () {
       expect(currentPath()).to.equal('palette')
-      return capture('palette', $('#ember-testing').width(), $('#ember-testing').height())
+      capture('palette', $('#ember-testing').width(), $('#ember-testing').height()).then(function (data) {
+        console.log(arguments)
+        done()
+      }).catch(function (err) {
+        done(err)
+      })
     })
   })
-  it('can visit /typography', function () {
+  it('can visit /typography', function (done) {
     visit('/typography')
 
     andThen(function () {
       expect(currentPath()).to.equal('typography')
-      return capture('typography', $('#ember-testing').width(), $('#ember-testing').height())
+      return capture('typography', $('#ember-testing').width(), $('#ember-testing').height()).then(function (data) {
+        console.log(arguments)
+        done()
+      }).catch(function (err) {
+        done(err)
+      })
     })
   })
-  it('can visit /icons', function () {
+  it('can visit /icons', function (done) {
     visit('/icons')
-
+    this.timeout(5000)
     andThen(function () {
       expect(currentPath()).to.equal('icons')
-      return capture('icons', $('#ember-testing').width(), $('#ember-testing').height(), 0.00, true)
+      return capture('icons', $('#ember-testing').width(), $('#ember-testing').height(), 0.00, true).then(function (data) {
+        console.log(arguments)
+        done()
+      }).catch(function (err) {
+        done(err)
+      })
     })
   })
-  it('can visit /area', function () {
+  it('can visit /area', function (done) {
     visit('/area')
 
     andThen(function () {
       expect(currentPath()).to.equal('area')
-      return capture('area', $('#ember-testing').width(), $('#ember-testing').height())
+      return capture('area', $('#ember-testing').width(), $('#ember-testing').height()).then(function (data) {
+        console.log(arguments)
+        done()
+      }).catch(function (err) {
+        done(err)
+      })
     })
   })
-  it('can visit /button', function () {
+  it('can visit /button', function (done) {
     visit('/button')
 
     andThen(function () {
       expect(currentPath()).to.equal('button')
-      return capture('button', $('#ember-testing').width(), $('#ember-testing').height())
+      return capture('button', $('#ember-testing').width(), $('#ember-testing').height()).then(function (data) {
+        console.log(arguments)
+        done()
+      }).catch(function (err) {
+        done(err)
+      })
     })
   })
-  it('can visit /checkbox', function () {
+  it('can visit /checkbox', function (done) {
     visit('/checkbox')
 
     andThen(function () {
       expect(currentPath()).to.equal('checkbox')
-      return capture('checkbox', $('#ember-testing').width(), $('#ember-testing').height())
+      return capture('checkbox', $('#ember-testing').width(), $('#ember-testing').height()).then(function (data) {
+        console.log(arguments)
+        done()
+      }).catch(function (err) {
+        done(err)
+      })
     })
   })
-  it('can visit /field', function () {
+  it('can visit /field', function (done) {
     visit('/field')
 
     andThen(function () {
       expect(currentPath()).to.equal('field')
-      return capture('field', $('#ember-testing').width(), $('#ember-testing').height())
+      return capture('field', $('#ember-testing').width(), $('#ember-testing').height()).then(function (data) {
+        console.log(arguments)
+        done()
+      }).catch(function (err) {
+        done(err)
+      })
     })
   })
-  it('can visit /password', function () {
+  it('can visit /password', function (done) {
     visit('/password')
 
     andThen(function () {
       expect(currentPath()).to.equal('password')
-      return capture('password', $('#ember-testing').width(), $('#ember-testing').height())
+      return capture('password', $('#ember-testing').width(), $('#ember-testing').height()).then(function (data) {
+        console.log(arguments)
+        done()
+      }).catch(function (err) {
+        done(err)
+      })
     })
   })
-  it('can visit /layout', function () {
+  it('can visit /layout', function (done) {
     visit('/layout')
 
     andThen(function () {
       expect(currentPath()).to.equal('layout')
-      return capture('layout', $('#ember-testing').width(), $('#ember-testing').height())
+      return capture('layout', $('#ember-testing').width(), $('#ember-testing').height()).then(function (data) {
+        console.log(arguments)
+        done()
+      }).catch(function (err) {
+        done(err)
+      })
     })
   })
-  it('can visit /link', function () {
+  it('can visit /link', function (done) {
     visit('/link')
 
     andThen(function () {
       expect(currentPath()).to.equal('link.index')
-      return capture('link', $('#ember-testing').width(), $('#ember-testing').height())
+      return capture('link', $('#ember-testing').width(), $('#ember-testing').height()).then(function (data) {
+        console.log(arguments)
+        done()
+      }).catch(function (err) {
+        done(err)
+      })
     })
   })
 })
