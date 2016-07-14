b0b78dc90d458e0843a32a039d23af97e5eee46b
Steven Glanzer <sglanzer@ciena.com>
2016-07-13 21:18:59 -0400

    Clear button working properly

--- addon/components/frost-password.js
+++ addon/components/frost-password.js
@@ -1,26 +1,26 @@
-import _ from 'lodash'
+// import _ from 'lodash'
 import Ember from 'ember'
-const {Component} = Ember
-import computed, {readOnly} from 'ember-computed-decorators'
-import layout from '../templates/components/frost-password'
+const {
+  next,
+  schedule
+} = Ember.run
+import FrostText from 'ember-frost-core/components/frost-text'
+// import computed, {readOnly} from 'ember-computed-decorators'
 
-export default Component.extend({
-  // ==========================================================================
-  // Dependencies
-  // ==========================================================================
+export default FrostText.extend({
 
-  // ==========================================================================
-  // Properties
-  // ==========================================================================
-
-  classNames: ['frost-password'],
-  classNameBindings: ['revealable'],
+  // == Properties ============================================================
+  attributeBindings: [
+    'type'
+  ],
+  classNames: [
+    'frost-password'
+  ],
+  // classNameBindings: ['revealable'],
   isCapsOn: false,
   isRevealerVisible: false,
-  layout,
   revealable: false,
   revealed: false,
-  revealIcon: 'show',
   tabindex: 0,
   type: 'password',
 
@@ -28,76 +28,126 @@ export default Component.extend({
   // Computed Properties
   // ==========================================================================
 
-  @readOnly
-  @computed('isCapsOn', 'isRevealerVisible', 'revealable')
-  isCapsAndReveal (isCapsOn, isRevealerVisible, revealable) {
-    return revealable && isCapsOn && isRevealerVisible
+  // @readOnly
+  // @computed('isCapsOn', 'isRevealerVisible', 'revealable')
+  // isCapsAndReveal (isCapsOn, isRevealerVisible, revealable) {
+  //   return revealable && isCapsOn && isRevealerVisible
+  // },
+
+  // == Event hooks ============================================================
+
+  didInsertElement() {
+    this._super(...arguments)
+
+    if (this.attrs.revealable) {
+      schedule('render', this, function () {
+        this.$()
+          .after(`
+            <svg class='frost-password-reveal' viewBox="0 0 21.6 20" xmlns="http://www.w3.org/2000/svg">
+              <g class='hidden'>
+                <path fill="#B3B3B3" d="M20.7,11.4c-3.6-4.1-7.3-6-10.8-5.7c-5.3,0.4-8.7,5.6-8.7,5.6l-1-0.7c0.2-0.2,3.8-5.7,9.6-6.2
+                  c4-0.3,8,1.7,11.9,6.1L20.7,11.4z"/>
+                <path fill="#B3B3B3" d="M10.7,5c-2.8,0-5.1,2.3-5.1,5.1s2.3,5.1,5.1,5.1c2.8,0,5.1-2.3,5.1-5.1S13.5,5,10.7,5z M13,10.1
+                  c-0.8,0-1.4-0.7-1.4-1.5c0-0.8,0.6-1.5,1.4-1.5s1.4,0.7,1.4,1.5C14.4,9.5,13.8,10.1,13,10.1z"/>
+              </g>
+              <g class='revealed'>
+                <circle fill="#B3B3B3" cx="10.7" cy="10" r="5.1"/>
+                <path fill="#B3B3B3" d="M20.7,11.3c-3.6-4.1-7.3-6-10.8-5.7C4.6,6,1.2,11.1,1.2,11.2l-1-0.7c0.2-0.2,3.8-5.7,9.6-6.2
+                  c4-0.3,8,1.7,11.9,6.1L20.7,11.3z"/>
+                <rect x="-0.5" y="8.9" transform="matrix(-0.6087 0.7934 -0.7934 -0.6087 26.2656 7.0071)" fill="#B3B3B3" width="23.8" height="2.2"/>
+                <path fill="#FFFFFF" d="M17.8,0.4L19,1.3L5,19.6l-1.1-0.9L17.8,0.4 M17.7-0.6L17.3,0l-14,18.3l-0.4,0.6l0.6,0.4l1.1,0.9l0.6,0.4
+                  L5.5,20l14-18.3L20,1.1l-0.6-0.4l-1.1-0.9L17.7-0.6L17.7-0.6z"/>
+              </g>
+            </svg>
+          `)
+        next(this, function () {
+          this.$().next('.frost-password-reveal').click(() => {
+            this.onReveal()
+          })
+          this.$().next().next('.frost-text-clear').click(() => {
+            this.onClear()
+          })
+        })
+      })
+    }
   },
 
-  // ==========================================================================
-  // Functions
-  // ==========================================================================
+  // == Functions ==============================================================
 
-  // ==========================================================================
-  // Events
-  // ==========================================================================
-
-  focusOut: Ember.on('focusOut', function () {
-    this.set('isCapsOn', false)
-  }),
-
-  keyDown: Ember.on('keyDown', function (e) {
-    var s = e || window.e
-    if (this.get('isCapsOn') === false && (s.which === 20 || s.keyCode === 20)) {
-      this.set('isCapsOn', true)
+  onReveal() {
+    this.toggleProperty('revealed')
+    this.set('type', this.get('revealed') ? 'text' : 'password')
+    const reveal = this.$().next('.frost-password-reveal')
+    if (this.get('revealed')) {
+      reveal.children('.revealed').show()
+      reveal.children('.hidden').hide()
+    } else {
+      reveal.children('.revealed').hide()
+      reveal.children('.hidden').show()
     }
-  }),
+  }
 
-  keyUp: Ember.on('keyUp', function (e) {
-    var s = e || window.e
-    if (this.get('isCapsOn') === true && (s.which === 20 || s.keyCode === 20)) {
-      this.set('isCapsOn', false)
-    }
-  }),
+  // == Events ================================================================
 
-  keyPressed: Ember.on('keyPress', function (e) {
-    var s = String.fromCharCode(e.which || e.keyCode)   // IE support
-    if ((s.toUpperCase() === s && s.toLowerCase() !== s && !e.shiftKey) ||
-      (s.toUpperCase() !== s && s.toLowerCase() === s && e.shiftKey)) { // caps is on
-      this.set('isCapsOn', true)
-    }
-  }),
 
-  _onFocus: Ember.on('focusIn', function (e) {
-    // If an onFocus handler is defined, call it
-    if (this.attrs.onFocus) {
-      this.attrs.onFocus()
-    }
-  }),
+
+  // focusOut: Ember.on('focusOut', function () {
+  //   this.set('isCapsOn', false)
+  // }),
+
+  // keyDown: Ember.on('keyDown', function (e) {
+  //   var s = e || window.e
+  //   if (this.get('isCapsOn') === false && (s.which === 20 || s.keyCode === 20)) {
+  //     this.set('isCapsOn', true)
+  //   }
+  // }),
+
+  // keyUp: Ember.on('keyUp', function (e) {
+  //   var s = e || window.e
+  //   if (this.get('isCapsOn') === true && (s.which === 20 || s.keyCode === 20)) {
+  //     this.set('isCapsOn', false)
+  //   }
+  // }),
+
+  // keyPressed: Ember.on('keyPress', function (e) {
+  //   var s = String.fromCharCode(e.which || e.keyCode)   // IE support
+  //   if ((s.toUpperCase() === s && s.toLowerCase() !== s && !e.shiftKey) ||
+  //     (s.toUpperCase() !== s && s.toLowerCase() === s && e.shiftKey)) { // caps is on
+  //     this.set('isCapsOn', true)
+  //   }
+  // }),
+
+  // _onFocus: Ember.on('focusIn', function (e) {
+  //   // If an onFocus handler is defined, call it
+  //   if (this.attrs.onFocus) {
+  //     this.attrs.onFocus()
+  //   }
+  // }),
 
   // ==========================================================================
   // Actions
   // ==========================================================================
 
-  actions: {
-    onBlur () {
-      const onBlur = this.get('onBlur')
+  // actions: {
+  //   onBlur () {
+  //     const onBlur = this.get('onBlur')
 
-      if (onBlur) {
-        onBlur()
-      }
-    },
+  //     if (onBlur) {
+  //       onBlur()
+  //     }
+  //   },
 
-    onInput (args) {
-      this.set('isRevealerVisible', args.value.length > 0)
-      if (_.isFunction(this.get('onInput'))) {
-        this.get('onInput')(args)
-      }
-    },
-    toggleReveal () {
-      this.toggleProperty('revealed')
-      this.set('type', this.get('revealed') ? 'text' : 'password')
-      this.set('revealIcon', this.get('revealed') ? 'hide' : 'show')
-    }
-  }
+  //   onInput (args) {
+  //     this.set('isRevealerVisible', args.value.length > 0)
+  //     if (_.isFunction(this.get('onInput'))) {
+  //       this.get('onInput')(args)
+  //     }
+  //   },
+  // }
 })
+
+//   {{#if isCapsAndReveal}}
+//     <div class='reveal-warning'>Caps on</div>
+//   {{else if isCapsOn}}
+//     <div class='warning'>Caps on</div>
+//   {{/if}}
--- addon/components/frost-text.js
+++ addon/components/frost-text.js
@@ -3,15 +3,19 @@ import FrostEvents from '../mixins/frost-events'
 import computed from 'ember-computed-decorators'
 
 const {
-  TextField
+  TextField,
+  isEmpty,
+  observer,
+  on,
+  run,
+  $
 } = Ember
-const {
-  next,
-  schedule
-} = Ember.run
 
 export default TextField.extend(FrostEvents, {
+
   // == Properties =============================================================
+
+  align: 'left',
   attributeBindings: [
     'style'
   ],
@@ -20,32 +24,66 @@ export default TextField.extend(FrostEvents, {
   ],
 
   // == Computed properties ====================================================
+
   @computed('align')
   style (align) {
     return Ember.String.htmlSafe(`text-align: ${CSS.escape(align)}`)
   },
 
-  // == Event hooks ============================================================
+  // == Events =================================================================
 
   didInsertElement() {
-    schedule('render', this, function () {
-      this.$().after(`
-        <svg class='frost-text-clear' fill="#000000" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+    run.schedule('render', this, function () {
+      const clearButton = $(`
+        <svg class='frost-text-clear' tabindex=-1 fill="#000000" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
           <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
           <path d="M0 0h24v24H0z" fill="none"/>
         </svg>
       `)
-      next(this, function () {
-        this.$().next().click(() => {
-          this.onClear()
-        })
+
+      clearButton.click(() => {
+        this.onClear()
       })
+
+      this.$().after(clearButton)
     })
   },
 
+  _focusIn: on('focusIn', function() {
+    this.set('_focused', true)
+  }),
+
+  _focusOut: on('focusOut', function(event) {
+    this.set('_focused', false)
+  }),
+
+  // == Observers  =============================================================
+
+  showClear: observer('value', '_focused', function() {
+    if (!this._focused) {
+      this.$().next('.frost-text-clear').css('opacity', 0)
+      run.later(this, function() {
+        this.$().next('.frost-text-clear').css('pointer-events', 'none')
+      }, 200)
+      return
+    }
+
+    if (isEmpty(this.value)) {
+      this.$().next('.frost-text-clear').css('opacity', 0)
+      run.later(this, function() {
+        this.$().next('.frost-text-clear').css('pointer-events', 'none')
+      }, 200)
+    } else {
+      this.$().next('.frost-text-clear').css('opacity', 1)
+      run.later(this, function() {
+        this.$().next('.frost-text-clear').css('pointer-events', 'auto')
+      }, 200)
+    }
+  }),
+
   // == Functions ==============================================================
 
-  onClear () {
+  onClear() {
     this.$().focus()
     this.$().val('')
     this.$().trigger('input')
