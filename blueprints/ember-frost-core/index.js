99b1e0bbb5cd38c36e333d928118dc0c6414da98
Matthew Dahl <dahl.matthew@gmail.com>
2016-08-01 20:19:57 -0700

    Switch to ember-lodash-shim

--- blueprints/ember-frost-core/index.js
+++ blueprints/ember-frost-core/index.js
@@ -11,7 +11,8 @@ module.exports = {
       'ember-frost-select',
       'ember-frost-text',
       'ember-frost-textarea',
-      'ember-frost-theme'
+      'ember-frost-theme',
+      'ember-lodash'
     ].map((packageName) => {
       return {name: packageName}
     })
@@ -37,7 +38,7 @@ module.exports = {
             {name: 'ember-concurrency', target: '^0.7.8'},
             {name: 'ember-computed-decorators', target: '>=0.2.2 <2.0.0'},
             {name: 'ember-hook', target: '1.2.1'},
-            {name: 'ember-lodash', target: '>=0.0.6 <2.0.0'},
+            {name: 'ember-lodash-shim', target: '>=0.1.0 <2.0.0'},
             {name: 'ember-one-way-controls', target: '>=0.5.3 <2.0.0'},
             {name: 'ember-truth-helpers', target: '^1.0.0'},
             {name: 'ember-redux', target: '^1.0.0'}
