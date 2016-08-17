d09f68a3935aaa09163116c9db2c93791bc02766
Steven Glanzer <sglanzer@ciena.com>
2016-08-17 10:51:48 -0400

    Adding a pre-transition onClick action to frost-link

--- tests/dummy/app/pods/link/controller.js
+++ tests/dummy/app/pods/link/controller.js
@@ -6,6 +6,17 @@ export default Controller.extend({
   fontSize: 20,
 
   actions: {
+    // BEGIN-SNIPPET pre-transition-action
+    preTransition() {
+      this.notifications.addNotification({
+        message: `Prior to transition...`,
+        type: 'success',
+        autoClear: true,
+        clearDuration: 2000
+      })
+    },
+    // END-SNIPPET
+
     increase (fontSize) {
       this.set('fontSize', fontSize + 1)
     },
