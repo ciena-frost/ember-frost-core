133f03d9ae668869373829ea523cc76cccb036c4
Jasonjys <jasonjys0511@gmail.com>
2016-09-14 16:39:23 -0400

    added intergration tests for disabled, error and placeholder

--- tests/integration/components/frost-text-test.js
+++ tests/integration/components/frost-text-test.js
@@ -36,9 +36,40 @@ describeComponent(
         .to.be.true
       expect($hook('my-text-input').hasClass('frost-text-input'))
         .to.be.true
-
       expect($hook('my-text-clear').hasClass('frost-text-clear'))
         .to.be.true
+    })
+
+    it('disabled', function () {
+      this.set('disabled', true)
+      this.render(hbs`{{frost-text disabled=disabled hook='my-text'}}`)
+
+      expect($hook('my-text-input').attr('disabled'))
+        .to.be.eql('disabled')
+
+      this.set('disabled', false)
+      expect($hook('my-text-input').attr('disabled'))
+        .to.be.undefined
+    })
+
+    it('error', function () {
+      this.set('error', 'error')
+      this.render(hbs`{{frost-text class=error hook='my-text'}}`)
+
+      expect($hook('my-text').hasClass('error'))
+        .to.be.true
+
+      this.set('error', 'change')
+      expect($hook('my-text').hasClass('error'))
+        .to.be.false
+    })
+
+    it('placeholder', function () {
+      this.set('placeholder', 'placeholder')
+      this.render(hbs`{{frost-text placeholder=placeholder hook='my-text'}}`)
+
+      expect($hook('my-text-input').attr('placeholder'))
+        .to.be.eql('placeholder')
     })
   }
 )
