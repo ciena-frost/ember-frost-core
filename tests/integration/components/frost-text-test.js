070883e8f4863104423316ab1b535e36174e441d
Jasonjys <jasonjys0511@gmail.com>
2016-09-16 16:17:47 -0400

    fixed focusin test

--- tests/integration/components/frost-text-test.js
+++ tests/integration/components/frost-text-test.js
@@ -3,6 +3,7 @@ import {describeComponent, it} from 'ember-mocha'
 import {beforeEach} from 'mocha'
 import hbs from 'htmlbars-inline-precompile'
 import {$hook, initialize} from 'ember-hook'
+import wait from 'ember-test-helpers/wait'
 
 describeComponent(
 	'frost-text',
@@ -18,7 +19,7 @@ describeComponent(
 		it('renders', function () {
 			this.render(hbs`
 				{{frost-text}}
-				`)
+			`)
 
 			expect(
 				this.$('input').attr('tabindex'),
@@ -55,7 +56,7 @@ describeComponent(
 				{{frost-text
 					align=align
 				}}
-				`)
+			`)
 
 			expect(
 				this.$('input').hasClass('right'),
@@ -68,7 +69,7 @@ describeComponent(
 				{{frost-text
 					autofocus=true
 				}}
-				`)
+			`)
 
 			expect(
 				this.$('input').attr('autofocus'),
@@ -85,7 +86,7 @@ describeComponent(
 				{{frost-text
 					value=value
 				}}
-				`)
+			`)
 
 			expect(
 				this.$('input').val(),
@@ -93,39 +94,61 @@ describeComponent(
 				).to.eql('Testing')
 		})
 
+    it('set disabled property', function() {
+        this.render(hbs`
+          {{frost-text
+            disabled=true
+          }}
+        `)
 
-		it('only renders the clear icon in insert', function (done) {
-			const value = 'Testing'
+        expect(
+          this.$('input').attr('disabled'),
+          'autofocus class is set'
+        ).to.eql('disabled')
+    })
+    
+		it('only renders the clear icon in insert', function () {
+      this.render(hbs`
+        {{frost-text
+          hook='test'
+        }}
+      `)
+      return wait().then(() => {
+        this.$('input').val('hello')
+        this.$('input').trigger('input')
+      })
 
-			this.set('value', value)
-
-			this.render(hbs`
-				{{frost-text value='Test'}}
-				`)
-
-				this.$('input').focus()
-
-
-		 // expect(
-		 //   this.$('.frost-text-clear'),
-		 //   'class "frost-text-clear" is set'
-		 // ).to.have.length(1)
-		Ember.run.next( () => {
-			debugger
-		 expect(
-		 	this.$('.is-clear-visible'),
-		 	'class is-clear-visible is set'
-		 	).to.have.length(1)
-			
-			done()
+      expect(
+        $hook('test').hasClass('is-clear-visible'),
+        'class "is-clear-visible" is set'
+      ).to.be.true
 		})
-	})
 
+    it('calls onFocus closure action', function () {
+      const externalActionSpy = sinon.spy()
+
+      this.on('externalAction', externalActionSpy)
+
+      this.render(hbs`
+        {{frost-checkbox
+          focusIn=(action 'externalAction')
+        }}
+      `)
+
+      this.$('input').trigger('focusin')
+
+      expect(
+        externalActionSpy.called,
+        'focusIn closure action called'
+      ).to.be.true
+    })
 
 		it('hook attr grabs frost-text as expected', function () {
 			this.render(hbs`
-				{{frost-text hook='my-text'}}
-				`)
+				{{frost-text 
+          hook='my-text'
+        }}
+			`)
 
 			expect(
 				$hook('my-text-input').hasClass('frost-text-input'),
@@ -138,16 +161,19 @@ describeComponent(
 				).to.be.true
 		})
 
-		it('sets error class', function(){
+		it('sets error class', function() {
+      this.set('error', 'error')
 			this.render(hbs`
 				{{frost-text
-					error=true}}
-				`)
+					class=error
+          hook='test'
+        }}
+			`)
+      //debugger
 			expect(
-				this.$('input').hasClass('error'),
-				'autofocus class is set'
+				$hook('test').hasClass('error'),
+				'error class is set'
 				).to.be.true
-
 		})
 	}
-	)
+)
