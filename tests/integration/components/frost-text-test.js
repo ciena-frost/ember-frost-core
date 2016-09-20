ad3ba934698ded1c333ffbf2b2b604b91cece697
Jasonjys <jasonjys0511@gmail.com>
2016-09-20 15:26:53 -0400

    fixed indentation of integration test

--- tests/integration/components/frost-text-test.js
+++ tests/integration/components/frost-text-test.js
@@ -7,112 +7,112 @@ import hbs from 'htmlbars-inline-precompile'
 import {$hook, initialize} from 'ember-hook'
 
 describeComponent(
-	'frost-text',
-	'Integration: FrostTextComponent',
-	{
-		integration: true
-	},
-	function () {
-		beforeEach(function () {
-			initialize()
-		})
-
-		it('renders', function () {
-			this.render(hbs`
-				{{frost-text}}
-			`)
-
-			expect(
-				this.$('input').attr('tabindex'),
-				'tabindex is set'
-				).to.eql('0')
-
-			expect(
-				this.$('input').attr('type'),
-				'type is set'
-				).to.eql('text')
-
-			expect(
-				this.$('input').hasClass('frost-text-input'),
-				'class "frost-text-input" is set'
-				).to.be.true
-
-			expect(
-				this.$('input').hasClass('left'),
-				'class "left" is set'
-				).to.be.true
-
-			expect(
-				this.$('.frost-text-clear'),
-				'class "frost-text-clear" is set'
-				).to.have.length(1)
-		})
-
-		it('sets align property', function () {
-			const align = 'right'
-
-			this.set('align', align)
-
-			this.render(hbs`
-				{{frost-text
-					align=align
-				}}
-			`)
-
-			expect(
-				this.$('input').hasClass('right'),
-				'class "right" is set'
-				).to.be.true
-		})
-
-		it('sets autofocus property', function () {
-			this.render(hbs`
-				{{frost-text
-					autofocus=true
-				}}
-			`)
-
-			expect(
-				this.$('input').attr('autofocus'),
-				'autofocus class is set'
-				).to.eql('autofocus')
-		})
-
-		it('sets value property', function () {
-			const value = 'Testing'
-
-			this.set('value', value)
-
-			this.render(hbs`
-				{{frost-text
-					value=value
-				}}
-			`)
-
-			expect(
-				this.$('input').val(),
-				'value is set'
-				).to.eql('Testing')
-		})
-
-    it('set disabled property', function() {
-        this.render(hbs`
-          {{frost-text
-            disabled=true
-          }}
-        `)
-
-        expect(
-          this.$('input').attr('disabled'),
-          'autofocus class is set'
-        ).to.eql('disabled')
+  'frost-text',
+  'Integration: FrostTextComponent',
+  {
+    integration: true
+  },
+  function () {
+    beforeEach(function () {
+        initialize()
     })
 
-		it('only renders the clear icon in insert', function () {
+    it('renders', function () {
+      this.render(hbs`
+          {{frost-text}}
+      `)
+
+      expect(
+        this.$('input').attr('tabindex'),
+        'tabindex is set'
+      ).to.eql('0')
+
+      expect(
+        this.$('input').attr('type'),
+        'type is set'
+      ).to.eql('text')
+
+      expect(
+        this.$('input').hasClass('frost-text-input'),
+        'class "frost-text-input" is set'
+      ).to.be.true
+
+      expect(
+        this.$('input').hasClass('left'),
+        'class "left" is set'
+      ).to.be.true
+
+      expect(
+        this.$('.frost-text-clear'),
+        'class "frost-text-clear" is set'
+      ).to.have.length(1)
+    })
+
+    it('sets align property', function () {
+      const align = 'right'
+
+      this.set('align', align)
+
+      this.render(hbs`
+          {{frost-text
+            align=align
+          }}
+      `)
+
+      expect(
+        this.$('input').hasClass('right'),
+        'class "right" is set'
+      ).to.be.true
+    })
+
+    it('sets autofocus property', function () {
+      this.render(hbs`
+          {{frost-text
+            autofocus=true
+          }}
+      `)
+
+      expect(
+        this.$('input').attr('autofocus'),
+        'autofocus class is set'
+      ).to.eql('autofocus')
+    })
+
+    it('sets value property', function () {
+      const value = 'Testing'
+
+      this.set('value', value)
+
       this.render(hbs`
         {{frost-text
+          value=value
         }}
       `)
+
+      expect(
+        this.$('input').val(),
+        'value is set'
+      ).to.eql('Testing')
+    })
+
+    it('set disabled property', function() {
+      this.render(hbs`
+        {{frost-text
+          disabled=true
+        }}
+      `)
+
+      expect(
+        this.$('input').attr('disabled'),
+        'autofocus class is set'
+      ).to.eql('disabled')
+    })
+
+    it('only renders the clear icon in insert', function () {
+      this.render(hbs`
+        {{frost-text}}
+      `)
+
       run(() => {
         this.$('input').val('hello')
         this.$('input').trigger('input')
@@ -127,7 +127,7 @@ describeComponent(
         this.$('.frost-text').hasClass('is-clear-enabled'),
         'class "is-clear-enabled" is set'
       ).to.be.true
-		})
+    })
 
     it('calls onFocus closure action', function () {
       const externalActionSpy = sinon.spy()
@@ -148,37 +148,37 @@ describeComponent(
       ).to.be.true
     })
 
-		it('hook attr grabs frost-text as expected', function () {
-			this.render(hbs`
-				{{frost-text 
-          hook='my-text'
-        }}
-			`)
+    it('hook attr grabs frost-text as expected', function () {
+      this.render(hbs`
+          {{frost-text 
+            hook='my-text'
+          }}
+      `)
 
-			expect(
-				$hook('my-text-input').hasClass('frost-text-input'),
-				'input hook is set'
-				).to.be.true
+      expect(
+        $hook('my-text-input').hasClass('frost-text-input'),
+        'input hook is set'
+      ).to.be.true
 
-			expect(
-				$hook('my-text-clear').hasClass('frost-text-clear'),
-				'clear hook is set'
-				).to.be.true
-		})
+      expect(
+        $hook('my-text-clear').hasClass('frost-text-clear'),
+        'clear hook is set'
+      ).to.be.true
+    })
 
-		it('sets error class', function() {
+    it('sets error class', function() {
       this.set('error', 'error')
-			this.render(hbs`
-				{{frost-text
-					class=error
+      this.render(hbs`
+        {{frost-text
+          class=error
           hook='test'
         }}
-			`)
+      `)
 
-			expect(
-				$hook('test').hasClass('error'),
-				'error class is set'
-				).to.be.true
-		})
-	}
+      expect(
+        $hook('test').hasClass('error'),
+        'error class is set'
+      ).to.be.true
+    })
+  }
 )
