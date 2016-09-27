bb4432d6a27044927fb22e31a34d14668d84836e
Michael Carroll <juwara0@users.noreply.github.com>
2016-09-27 08:40:08 -0500

    cleanup on frost-radio-button

--- addon/components/frost-radio-button.js
+++ addon/components/frost-radio-button.js
@@ -1,11 +1,15 @@
+import computed from 'ember-computed-decorators'
 import Ember from 'ember'
 const {
+  $,
+  assert,
   Component,
-  computed
+  computed: {
+    readOnly
+  },
+  get,
+  typeOf
 } = Ember
-const {
-  readOnly
-} = computed
 import PropTypeMixin, {PropTypes} from 'ember-prop-types'
 import layout from '../templates/components/frost-radio-button'
 
@@ -44,20 +48,33 @@ export default Component.extend(PropTypeMixin, {
   groupValue: readOnly('parentView.value'),
   onChange: readOnly('parentView.onChange'),
 
-  checked: computed('groupValue', 'value', function () {
-    return this.get('groupValue') === this.get('value')
-  }),
+  @computed('groupValue', 'value')
+  /**
+   * Determine state of checked
+   * @param {String} groupValue - which radio button in the group is set
+   * @param {String} value - is this radio button selected
+   * @returns {Boolean} whether this radio button is checked
+   */
+  checked (groupValue, value) {
+    return groupValue === value
+  },
 
-  tabindex: Ember.computed('disabled', function () {
-    return this.get('disabled') ? -1 : 0
-  }),
+  @computed('disabled')
+  /**
+   * Determine tabindex setting
+   * @param {String} disabled - component disabled state
+   * @returns {String} whether this radio button is checked
+   */
+  tabindex (disabled) {
+    return disabled ? '-1' : '0'
+  },
 
   // == Functions ===============================================================
 
   _createEvent (_event, _target) {
-    let event = Ember.$.Event(null, _event)
-    let target = Ember.$.clone(_target)
-    target.id = this.get('groupId')
+    let event = $.Event(null, _event)
+    let target = $.clone(_target)
+    target.id = get(this, 'groupId')
     event.target = target
     return event
   },
@@ -70,30 +87,30 @@ export default Component.extend(PropTypeMixin, {
   },
 
   _setupAssertions () {
-    Ember.assert(
+    assert(
       `${this.toString()} must be initialized in the yield block of 'frost-radio-group'`,
       /frost-radio-group/.test(this.parentView.toString()))
-    Ember.assert(
+    assert(
       `${this.toString()} must be initialized with a 'value' property`,
-      this.get('value')
+      get(this, 'value')
     )
   },
 
   keyPress (e) {
     if (e.keyCode === 13 || e.keyCode === 32) {
-      if (this.get('disabled') || this.get('groupValue') === this.get('value')) {
+      if (get(this, 'disabled') || get(this, 'groupValue') === get(this, 'value')) {
         return
       }
-      let change = this.get('onChange')
-      if (change && typeof change === 'function') {
-        change(this._createEvent(e, Ember.$(e.target).find('input')[0]))
+      let change = get(this, 'onChange')
+      if (change && typeOf(change) === 'function') {
+        change(this._createEvent(e, $(e.target).find('input')[0]))
       }
     }
   },
 
   change (event) {
-    const onChange = this.get('onChange')
-    if (onChange && typeof onChange === 'function') {
+    const onChange = get(this, 'onChange')
+    if (onChange && typeOf(onChange) === 'function') {
       onChange(this._createEvent(event, event.target))
     }
   }
