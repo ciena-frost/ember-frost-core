dd1fccc6d46af2f5c8081e295fb8bd77156fe0ab
Matthew Dahl <dahl.matthew@gmail.com>
2016-10-11 11:05:14 -0700

    Open select up when on bottom half of window

--- addon/components/frost-select-dropdown.js
+++ addon/components/frost-select-dropdown.js
@@ -1,5 +1,5 @@
 import Ember from 'ember'
-const {Component, get} = Ember
+const {$, Component, get} = Ember
 import computed, {readOnly} from 'ember-computed-decorators'
 import PropTypeMixin, {PropTypes} from 'ember-prop-types'
 
@@ -35,11 +35,20 @@ export default Component.extend(PropTypeMixin, {
   },
 
   @readOnly
-  @computed('left', 'top', 'width')
-  listStyle (left, top, width) {
+  @computed('bottom', 'left', 'top', 'width')
+  listStyle (bottom, left, top, width) {
+    if (bottom !== 'auto') {
+      bottom = `${bottom}px`
+    }
+
+    if (top !== 'auto') {
+      top = `${top}px`
+    }
+
     const style = [
+      `bottom:${bottom}`,
       `left:${left}px`,
-      `top:${top}px`,
+      `top:${top}`,
       `width:${width}px`
     ]
       .join(';')
@@ -47,21 +56,59 @@ export default Component.extend(PropTypeMixin, {
     return Ember.String.htmlSafe(style)
   },
 
-  /* eslint-disable complexity */
-  didReceiveAttrs (attrs) {
-    const element = get(attrs, 'newAttrs.element.value').first()
-    const offset = element.offset()
-    const props = {}
-    const left = offset.left
-    const top = offset.top + element.height() + UP_ARROW_HEIGHT + BORDER_HEIGHT
-    const width = element.width()
+  _getElementDimensionsAndPosition ($element) {
+    const height = $element.height()
+    const offset = $element.offset()
+    const top = offset.top
+
+    return {
+      center: {
+        x: height / 2 + top
+      },
+      height,
+      left: offset.left,
+      top,
+      width: $element.width()
+    }
+  },
+
+  _positionAboveInput (top) {
+    const bottom = $(window).height() - top + $(document).scrollTop()
+
+    if (bottom === this.get('bottom')) {
+      return {}
+    }
+
+    return {
+      bottom,
+      top: 'auto'
+    }
+  },
+
+  _positionBelowInput (height, top) {
+    // Make sure dropdown is rendered below input and we leave space for arrow
+    // that connects dropdown to input
+    top = top + height + UP_ARROW_HEIGHT + BORDER_HEIGHT - $(document).scrollTop()
+
+    if (top === this.get('top')) {
+      return {}
+    }
+
+    return {
+      bottom: 'auto',
+      top
+    }
+  },
+
+  _updatePosition ($element) {
+    const {center, height, left, top, width} = this._getElementDimensionsAndPosition($element)
+    const windowCenterX = $(window).height() / 2 + $(document).scrollTop()
+    const props = (
+      center.x > windowCenterX ? this._positionAboveInput(top) : this._positionBelowInput(height, top)
+    )
 
     if (left !== this.get('left')) {
       props.left = left
-    }
-
-    if (top !== this.get('top')) {
-      props.top = top
     }
 
     if (width !== this.get('width')) {
@@ -71,6 +118,28 @@ export default Component.extend(PropTypeMixin, {
     if (Object.keys(props).length !== 0) {
       this.setProperties(props)
     }
+  },
+
+  didReceiveAttrs (attrs) {
+    const $element = get(attrs, 'newAttrs.element.value').first()
+    this._updatePosition($element)
+  },
+
+  didInsertElement () {
+    this._updateHandler = () => {
+      const $element = get(this.attrs, 'element.value')
+
+      if ($element) {
+        this._updatePosition($element.first())
+      }
+    }
+
+    $(window).on('resize', this._updateHandler)
+    $(document).on('scroll', this._updateHandler)
+  },
+
+  willDestroyElement () {
+    $(window).off('resize', this._updateHandler)
+    $(document).off('scroll', this._updateHandler)
   }
-  /* eslint-enable complexity */
 })
