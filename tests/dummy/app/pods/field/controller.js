84534865895969031abde572f54282e4beb002fb
Matthew Dahl <dahl.matthew@gmail.com>
2017-01-03 16:45:05 -0800

    Address another PR comment

--- tests/dummy/app/pods/field/controller.js
+++ tests/dummy/app/pods/field/controller.js
@@ -1,7 +1,5 @@
 import Ember from 'ember'
-const {
-  Controller, inject
-} = Ember
+const {Controller, inject} = Ember
 
 export default Controller.extend({
   notifications: inject.service('notification-messages'),
