6c35094e1a247a0a08c51899c3f68fd463a3e980
dafortin@ciena.com <dafortin@ciena.com>
2017-04-03 13:57:25 -0400

    Added blueprint helper

---
+++ blueprint-helper.js
@@ -0,0 +1,9 @@
+var consumer = require('ember-frost-core/lib/utils/consumer')
+var npm = require('ember-frost-core/lib/utils/npm')
+var packageHandler = require('ember-frost-core/lib/utils/package-handler')
+
+module.exports = {
+  consumer,
+  npm,
+  packageHandler
+}
--- blueprints/ember-frost-core/index.js
+++ blueprints/ember-frost-core/index.js
@@ -1,9 +1,23 @@
+const blueprintHelper = require('ember-frost-core/blueprint-helper')
+
 module.exports = {
-  afterInstall: function () {
-    return this.addAddonsToProject({
-      packages: [
-        {name: 'ember-concurrency', target: '~0.7.15'}
-      ]
+  afterInstall: function (options) {
+    const addonsToAdd = [
+      {name: 'ember-computed-decorators', target: '~0.2.0'},
+      {name: 'ember-concurrency', target: '~0.7.15'},
+      {name: 'ember-elsewhere', target: '~0.4.1'},
+      {name: 'ember-hook', target: '^1.4.1'},
+      {name: 'ember-prop-types', target: '^3.0.0'},
+      {name: 'ember-spread', target: '^1.1.1'},
+      {name: 'ember-truth-helpers', target: '^1.2.0'}
+    ]
+
+    const consumerPackages = blueprintHelper.consumer.getPackages(options)
+
+    return blueprintHelper.packageHandler.getPkgsToInstall(addonsToAdd, consumerPackages).then((pkgsToInstall) => {
+      if (pkgsToInstall.length !== 0) {
+        return this.addAddonsToProject({packages: pkgsToInstall})
+      }
     })
   },
 
