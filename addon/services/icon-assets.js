737667ece745d69ccb5c6375b24ed2c569121bd2
Seena <seenarowhani95@gmail.com>
2017-04-02 20:35:28 -0400

    async

--- addon/services/icon-assets.js
+++ addon/services/icon-assets.js
@@ -1,38 +1,42 @@
 import Ember from 'ember'
-const { Service } = Ember
+const { inject, Service } = Ember
 
-const ICON_PATH = 'assets/icon-packs'
-const ICON_ASSETS = 'assets/icon-assets.json'
+const iconPath = 'assets/icon-packs'
+const iconAssets = 'assets/icon-assets.json'
+
 export default Service.extend({
+  ajax: inject.service(),
   _icons: [],
   init () {
     this._super(...arguments)
-    Ember.$.getJSON(ICON_ASSETS).then((assetMap) => {
-      this._assets = assetMap.assets
-      this._icons.forEach(icon => {
-        const pack = icon.get('pack')
-        const href = this._assets[`${ICON_PATH}/${pack}.svg`]
-        icon.get('iconTask').perform(href)
-      })
-    }).fail(() => {
-      this._icons.forEach(icon => {
-        const pack = icon.get('pack')
-        const href = `${ICON_PATH}/${pack}.svg`
-        icon.get('iconTask').perform(href)
-      })
-    }).always(() => delete this._icons)
+
+    this.get('ajax')
+      .request(iconAssets)
+      .then((result) => {this._assets = result.assets})
+      .finally(() => this._loadIcons())
   },
-  register (icon) {
-    const pack = icon.get('pack')
-    let href
-    if (this._assets) {
-      href = this._assets[`${ICON_PATH}/${pack}.svg`]
-    } else if (this._icons !== undefined) { // assets have yet to resolve
-      this._icons.push(icon) // queue it up
-      return
-    } else {
-      href = `${ICON_PATH}/${pack}.svg`
+  register (element) {
+    const path = `${iconPath}/${element.get('pack')}.svg`
+
+    const icon = {
+      element,
+      path
     }
-    icon.get('iconTask').perform(href)
+
+    if (this._icons !== undefined) { // assets have yet to resolve
+      this._icons.push(icon)
+    } else {
+      this._load(icon)
+    }
+  },
+  _loadIcons () {
+    const assets = this._assets || {}
+    this._icons.forEach(el => this._load(el))
+    delete this._icons
+  },
+  _load (icon) {
+    const {element, path} = icon
+    const href = this._assets[path] || path
+    element.get('iconTask').perform(href)
   }
 })
