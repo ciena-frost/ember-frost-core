99123a86da6ec0b3575f9759c5acdef6edc5f85e
Adam Meadows <adam.meadows@gmail.com>
2017-04-25 16:17:08 -0600

    Refactor frost-toggle-test

--- tests/integration/components/frost-toggle-test.js
+++ tests/integration/components/frost-toggle-test.js
@@ -1,45 +1,46 @@
 import {expect} from 'chai'
 import {$hook} from 'ember-hook'
+import wait from 'ember-test-helpers/wait'
 import {integration} from 'ember-test-utils/test-support/setup-component-test'
 import hbs from 'htmlbars-inline-precompile'
-import {describe, it} from 'mocha'
+import {before, beforeEach, describe, it} from 'mocha'
 import sinon from 'sinon'
 
 const test = integration('frost-toggle')
 describe(test.label, function () {
   test.setup()
 
-  it('renders default values', function () {
-    this.render(hbs`
-      {{frost-toggle hook='myToggle'}}
-    `)
+  describe('when rendered with defaults', function () {
+    beforeEach(function () {
+      this.render(hbs`
+        {{frost-toggle hook='myToggle'}}
+      `)
 
-    expect(
-      this.$('.frost-toggle').find('input').prop('type'),
-      'type set to "checkbox"'
-    ).to.eql('checkbox')
+      return wait()
+    })
 
-    expect(
-      this.$('.frost-toggle-input'),
-      'class frost-toggle-input is set'
-    ).to.have.length(1)
+    it('should default the "type" to "checkbox"', function () {
+      expect(this.$('.frost-toggle input')).to.have.prop('type', 'checkbox')
+    })
 
-    expect(
-      this.$('.frost-toggle').find('label').hasClass('frost-toggle-button'),
-      'label has class "frost-toggle-button"'
-    ).to.equal(true)
+    it('should render a .frost-toggle-input element', function () {
+      expect(this.$('.frost-toggle-input')).to.have.length(1)
+    })
 
-    expect(
-      this.$('.frost-toggle').find('label').prop('for'),
-      '"label for" property has the correct value'
-    ).to.eql(
-      this.$('.frost-toggle').find('input').prop('id')
-    )
+    it('should put the "frost-toggle-button" class on the label', function () {
+      expect(this.$('.frost-toggle label')).to.have.class('frost-toggle-button')
+    })
+
+    it('should set proper "for" property on the label', function () {
+      const id = this.$('.frost-toggle input').prop('id')
+      expect(this.$('.frost-toggle label')).to.have.prop('for', id)
+    })
   })
 
-  it('throws assertion error', function () {
-    expect(
-      () => {
+  describe('when trueValue and falseValue are the same', function () {
+    let renderIt
+    beforeEach(function () {
+      renderIt = () => {
         this.render(hbs`
           {{frost-toggle
             hook='myToggle'
@@ -47,199 +48,229 @@ describe(test.label, function () {
             falseValue='testValue'
           }}
         `)
-      },
-      'assertion thrown when trueValue and falseValue are same value'
-    ).to.throw(/Same value has been assigned/)
+      }
+    })
+
+    it('should throw an error', function () {
+      expect(renderIt).to.throw(/Same value has been assigned/)
+    })
   })
 
-  it('sets disabled property', function () {
-    this.render(hbs`
-      {{frost-toggle
-        disabled=true
-        hook='myToggle'
-      }}
-   `)
+  describe('when disabled property is passed in', function () {
+    beforeEach(function () {
+      this.render(hbs`
+        {{frost-toggle
+          disabled=true
+          hook='myToggle'
+        }}
+      `)
 
-    expect(
-      this.$('.frost-toggle').find('input').prop('disabled'),
-      'disabled is set'
-    ).to.equal(true)
+      return wait()
+    })
+
+    it('should set the disabled property on the input', function () {
+      expect(this.$('.frost-toggle input')).to.have.prop('disabled', true)
+    })
   })
 
-  it('sets hook property', function () {
-    this.render(hbs`
-      {{frost-toggle
-        hook='my-test'
-      }}
-   `)
+  describe('when hook is passed in', function () {
+    beforeEach(function () {
+      this.render(hbs`
+        {{frost-toggle
+          hook='ft'
+        }}
+      `)
 
-    expect(
-      $hook('my-test-toggle-input').hasClass('frost-toggle-input'),
-      'input hook is set'
-    ).to.equal(true)
+      return wait()
+    })
 
-    expect(
-      $hook('my-test-toggle-label').hasClass('frost-toggle-button'),
-      'label hook is set'
-    ).to.equal(true)
+    it('should add a hook to the input', function () {
+      expect($hook('ft-toggle-input')).to.have.class('frost-toggle-input')
+    })
 
-    expect(
-      $hook('my-test-toggle-text-on').hasClass('frost-toggle-text on'),
-      'toggle text on hook is set'
-    ).to.equal(true)
+    it('should add a hook to the label', function () {
+      expect($hook('ft-toggle-label')).to.have.class('frost-toggle-button')
+    })
 
-    expect(
-      $hook('my-test-toggle-text-off').hasClass('frost-toggle-text off'),
-      'toggle text off hook is set'
-    ).to.equal(true)
+    it('should add a hook to the on text', function () {
+      expect($hook('ft-toggle-text-on')).to.have.class('frost-toggle-text on')
+    })
+
+    it('should add a hook to the off text', function () {
+      expect($hook('ft-toggle-text-off')).to.have.class('frost-toggle-text off')
+    })
   })
 
-  it('sets value property', function () {
-    const value = 'test value'
+  describe('when a value is passed in', function () {
+    beforeEach(function () {
+      this.render(hbs`
+        {{frost-toggle
+          hook='myToggle'
+          value='test value'
+        }}
+      `)
 
-    this.set('value', value)
+      return wait()
+    })
 
-    this.render(hbs`
-      {{frost-toggle
-        hook='myToggle'
-        value=value
-      }}
-   `)
-
-    expect(
-      this.$('.frost-toggle').find('input').val(),
-      'value is set'
-    ).to.eql('on')
+    it('should start out on', function () {
+      expect(this.$('.frost-toggle input')).to.have.value('on')
+    })
   })
 
-  describe('label is set', function () {
-    it('uses trueLabel property', function () {
+  describe('when passing in a trueLabel', function () {
+    beforeEach(function () {
       this.render(hbs`
         {{frost-toggle
           hook='myToggle'
           trueLabel='Label On'
           value='on'
         }}
-     `)
+      `)
 
-      expect(
-        this.$('.on').text().trim(),
-        'trueLabel is set'
-      ).to.eql('Label On')
+      return wait()
     })
 
-    it('uses default on label of "On"', function () {
-      this.render(hbs`
-        {{frost-toggle
-          hook='myToggle'
-          value='on'
-        }}
-     `)
-
-      expect(
-        this.$('.on').text().trim(),
-        'default on label is set'
-      ).to.eql('On')
-    })
-
-    it('uses falseLabel property', function () {
-      this.render(hbs`
-        {{frost-toggle
-          falseLabel='Label Off'
-          hook='myToggle'
-          value='on'
-        }}
-     `)
-
-      expect(
-        this.$('.off').text().trim(),
-        'falseLabel is set'
-      ).to.eql('Label Off')
-    })
-
-    it('uses default off label of "Off"', function () {
-      this.render(hbs`
-        {{frost-toggle hook='myToggle'}}
-     `)
-
-      expect(
-        this.$('.off').text().trim(),
-        'default off label is set'
-      ).to.eql('Off')
+    it('should use the passed in label text', function () {
+      expect(this.$('.on')).to.have.text('Label On')
     })
   })
 
-  it('uses trueValue property', function () {
-    this.set('testValue', 'enabled')
+  describe('when not passing in a trueLabel', function () {
+    beforeEach(function () {
+      this.render(hbs`
+        {{frost-toggle
+          hook='myToggle'
+          value='on'
+        }}
+      `)
 
-    this.render(hbs`
+      return wait()
+    })
+
+    it('should use the default in label text', function () {
+      expect(this.$('.on')).to.have.text('On')
+    })
+  })
+
+  describe('when passing in a falseLabel', function () {
+    beforeEach(function () {
+      this.render(hbs`
+        {{frost-toggle
+          hook='myToggle'
+          falseLabel='Label Off'
+          value='off'
+        }}
+      `)
+
+      return wait()
+    })
+
+    it('should use the passed in label text', function () {
+      expect(this.$('.off')).to.have.text('Label Off')
+    })
+  })
+
+  describe('when not passing in a falseLabel', function () {
+    beforeEach(function () {
+      this.render(hbs`
+        {{frost-toggle
+          hook='myToggle'
+          value='off'
+        }}
+      `)
+
+      return wait()
+    })
+
+    it('should use the default in label text', function () {
+      expect(this.$('.off')).to.have.text('Off')
+    })
+  })
+
+  describe('when trueValue is passed in', function () {
+    beforeEach(function () {
+      this.render(hbs`
         {{frost-toggle
           hook='myToggle'
           trueValue='enabled'
-          value=testValue
+          value='enabled'
         }}
-     `)
+      `)
 
-    expect(
-      this.$('input').prop('checked'),
-      'toggle is in "On" state'
-    ).to.equal(true)
+      return wait()
+    })
+
+    it('recognizes the true value and sets the On state', function () {
+      expect(this.$('input')).to.have.prop('checked')
+    })
   })
 
-  it('uses falseValue property', function () {
-    this.set('testValue', 'disabled')
-
-    this.render(hbs`
+  describe('when falseValue is passed in', function () {
+    beforeEach(function () {
+      this.render(hbs`
         {{frost-toggle
+          hook='myToggle'
+          falseValue='disabled'
+          value='disabled'
+        }}
+      `)
+
+      return wait()
+    })
+
+    it('recognizes the false value and sets the Off state', function () {
+      expect(this.$('input')).to.have.prop('checked', false)
+    })
+  })
+
+  describe('when given a closure action', function () {
+    let stub
+    beforeEach(function () {
+      stub = sinon.stub()
+      this.on('myAction', stub)
+
+      this.render(hbs`
+        {{frost-toggle
+          trueValue='enabled'
           falseValue='disabled'
           hook='myToggle'
-          value=testValue
+          value='disabled'
+          onClick=(action 'myAction')
         }}
-     `)
+      `)
 
-    expect(
-      this.$('input').prop('checked'),
-      'toggle is in "Off" state'
-    ).to.equal(false)
+      return wait()
+    })
+
+    describe('when the label is clicked', function () {
+      beforeEach(function () {
+        this.$('label').click()
+        return wait()
+      })
+
+      it('should call the action', function () {
+        expect(stub).to.have.callCount(1)
+      })
+    })
   })
 
-  it('fires onClick closure action', function () {
-    const externalActionSpy = sinon.spy()
-
-    this.on('externalAction', externalActionSpy)
-
-    this.set('testValue', 'disabled')
-
-    this.render(hbs`
+  describe('when rendering using spread', function () {
+    beforeEach(function () {
+      this.render(hbs`
         {{frost-toggle
-          falseValue='disabled'
-          hook='myToggle'
-          value=testValue
-          onClick=(action 'externalAction')
+          options=(hash
+            disabled=true
+            hook='myToggle'
+          )
         }}
-     `)
+      `)
 
-    this.$('label').click()
+      return wait()
+    })
 
-    expect(
-      externalActionSpy.called,
-      'onClick closure action called'
-    ).to.equal(true)
-  })
-
-  it('renders using spread', function () {
-    this.render(hbs`
-      {{frost-toggle
-        options=(hash
-          disabled=true
-          hook='myToggle'
-        )
-      }}
-   `)
-
-    expect(
-      this.$('.frost-toggle').find('input').prop('disabled'),
-      'disabled is set'
-    ).to.equal(true)
+    it('should pass along the disabled property', function () {
+      expect(this.$('.frost-toggle input')).to.have.prop('disabled', true)
+    })
   })
 })
