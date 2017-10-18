f77c420deddf9e631d2b70d671466362d3e270d6
Michael Carroll <juwara0@users.noreply.github.com>
2017-10-18 07:21:01 -0500

    added two missing files created as a result of runnign ember init

---
+++ tests/helpers/module-for-acceptance.js
@@ -0,0 +1,21 @@
+import destroyApp from '../helpers/destroy-app'
+import startApp from '../helpers/start-app'
+import {module} from 'qunit'
+import {resolve} from 'rsvp'
+
+export default function (name, options = {}) {
+  module(name, {
+    beforeEach () {
+      this.application = startApp()
+
+      if (options.beforeEach) {
+        return options.beforeEach.apply(this, arguments)
+      }
+    },
+
+    afterEach () {
+      let afterEach = options.afterEach && options.afterEach.apply(this, arguments)
+      return resolve(afterEach).then(() => destroyApp(this.application))
+    }
+  })
+}
