d5f47811ba2eb9100fae46a2608ed613c678de40
Jeremy Brown <jeremy@notmessenger.com>
2017-12-06 11:39:20 -0600

    Remove unneeded blueprints folder

--- blueprints/ember-frost-core/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  normalizeEntityName: function () {
-    // this prevents an error when the entityName is
-    // not specified (since that doesn't actually matter to us)
-  }
-}
