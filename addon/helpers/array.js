c9d6ca7cf5a1885b59439ffef57644c683623b15
Michael Carroll <juwara0@users.noreply.github.com>
2018-02-27 12:03:02 -0600

    update array helper to use .slice to make new array

--- addon/helpers/array.js
+++ addon/helpers/array.js
@@ -3,13 +3,12 @@
  * Temporary fix until the following bug is resolved: https://github.com/emberjs/ember.js/issues/14264
  */
 import Ember from 'ember'
-const {A, Helper} = Ember
+const {A: emberA, Helper} = Ember
 const {helper} = Helper
 
 export function array (params) {
-  let array = A()
-  array.pushObjects(params)
-  return array
+  // params is a frozen, non-ember array
+  return emberA(params.slice())
 }
 
 export default helper(array)
