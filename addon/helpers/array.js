e79e86d25d8107e4be0b4d11c11af8b62e5ffc38
Michael Carroll <juwara0@users.noreply.github.com>
2018-03-09 14:55:06 -0600

    update alias for ember array

--- addon/helpers/array.js
+++ addon/helpers/array.js
@@ -3,12 +3,12 @@
  * Temporary fix until the following bug is resolved: https://github.com/emberjs/ember.js/issues/14264
  */
 import Ember from 'ember'
-const {A: emberA, Helper} = Ember
+const {A: emberArray, Helper} = Ember
 const {helper} = Helper
 
 export function array (params) {
   // params is a frozen, non-ember array
-  return emberA(params.slice())
+  return emberArray(params.slice())
 }
 
 export default helper(array)
